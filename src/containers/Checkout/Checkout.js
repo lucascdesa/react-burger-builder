13bdaec1700bb4e5f9b9074fea80672eabcc7809
Lucas Cardoso de Sa <lucascdesa@hotmail.com>
2018-04-09 22:05:18 +1200

    Passing props to summary

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,32 @@ class checkout extends Component {
             bacon: 1
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            //['salad', '1']
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ ingredients: ingredients });
+    }
+
+    checkoutCancelled = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinued = () => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelled}
+                    checkoutContinued={this.checkoutContinued} />
             </div>
         );
     }
